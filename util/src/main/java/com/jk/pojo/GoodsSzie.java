130d902557962087fa15737ca3a62247af1b2869
lzg <https://github.com/lanmojun/pinyougou.git>
2018-05-09 14:00:57 +0800

    刘振国，在util模块中，新建了五个实体类订单、客户、以及商品相关的实体类！

---
+++ util/src/main/java/com/jk/pojo/GoodsSzie.java
@@ -0,0 +1,40 @@
+package com.jk.pojo;
+
+import javax.persistence.*;
+
+@Entity
+@Table(name = "pyg_goodsSize")
+public class GoodsSzie {
+
+    @Id
+    @Column(name = "goodsSizeId")
+    @GeneratedValue(strategy = GenerationType.AUTO)
+    private  Integer goodsSizeId;
+
+    @Column(name = "goodsSizeName")
+    private String goodsSizeName;
+
+    @Override
+    public String toString() {
+        return "GoodsSzie{" +
+                "goodsSizeId=" + goodsSizeId +
+                ", goodsSizeName='" + goodsSizeName + '\'' +
+                '}';
+    }
+
+    public Integer getGoodsSizeId() {
+        return goodsSizeId;
+    }
+
+    public void setGoodsSizeId(Integer goodsSizeId) {
+        this.goodsSizeId = goodsSizeId;
+    }
+
+    public String getGoodsSizeName() {
+        return goodsSizeName;
+    }
+
+    public void setGoodsSizeName(String goodsSizeName) {
+        this.goodsSizeName = goodsSizeName;
+    }
+}
