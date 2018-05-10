84095e0079f2449075eb554dd8dd263e641f8ba2
Mrzhang <313021983@qq.com>
2018-05-10 19:53:49 +0800

    merchantDao

---
+++ parent/zq/src/main/java/com/jk/dao/MerchantDao.java
@@ -0,0 +1,16 @@
+package com.jk.dao;
+
+import java.util.List;
+
+/**
+ * Created by Administrator on 2018/5/10.
+ */
+public interface MerchantDao {
+    List queryMerchant();
+
+    void deleteMerchant(Integer merchantId);
+
+    void updateSucces(String hql);
+
+    void updateErre(String hql);
+}
