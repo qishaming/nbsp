9c37fc6640193cdc53ba62cd6c506fbd6d10976a
Mrzhang <313021983@qq.com>
2018-05-10 19:54:18 +0800

    merchantDaoImpl

---
+++ parent/zq/src/main/java/com/jk/dao/impl/MerchantDaoImpl.java
@@ -0,0 +1,37 @@
+package com.jk.dao.impl;
+
+import com.jk.dao.MerchantDao;
+import org.hibernate.SessionFactory;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Repository;
+
+import java.util.List;
+
+/**
+ * Created by Administrator on 2018/5/10.
+ */
+@Repository
+public class MerchantDaoImpl implements MerchantDao {
+
+    @Autowired
+    private SessionFactory sessionFactory;
+
+    public List queryMerchant() {
+        String hql="from  Merchant where merchantAuditState=0 ";
+        List list= sessionFactory.openSession().createQuery(hql).list();
+        return  list;
+    }
+
+    public void deleteMerchant(Integer merchantId) {
+      String hql="delete from  Merchant where merchantId= "+merchantId;
+        sessionFactory.getCurrentSession().createQuery(hql).executeUpdate();
+    }
+
+    public void updateSucces(String hql) {
+        sessionFactory.getCurrentSession().createQuery(hql).executeUpdate();
+    }
+
+    public void updateErre(String hql) {
+        sessionFactory.getCurrentSession().createQuery(hql).executeUpdate();
+    }
+}
