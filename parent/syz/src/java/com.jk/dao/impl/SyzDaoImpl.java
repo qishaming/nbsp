2f06740eb62e72303fd7c166d6647f021e07d195
sunsyz <1246307448@qq.com>
2018-05-17 22:36:50 +0800

    syz提交的代码

--- parent/syz/src/java/com.jk/dao/impl/SyzDaoImpl.java
+++ parent/syz/src/java/com.jk/dao/impl/SyzDaoImpl.java
@@ -1,11 +1,14 @@
 package com.jk.dao.impl;
 
 import com.jk.dao.SyzDao;
+import com.jk.pojo.*;
+import org.hibernate.Session;
 import org.hibernate.SessionFactory;
 import org.hibernate.transform.Transformers;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Repository;
 
+import java.text.SimpleDateFormat;
 import java.util.List;
 
 @Repository
@@ -13,18 +16,65 @@ public class SyzDaoImpl implements SyzDao {
     @Autowired
     private SessionFactory SessionFactory;
 
-    public List queryGoods() {
-        String hql="select t1.goodsid as goodsid ,t1.goodsname as goodsname ,t1.goodsimg as goodsimg, t1.goodsPrice as goodsPrice,t1.goodsDiscounts as goodsDiscounts, t2.brandName as brandName,t3.goodsSizeName as goodsSizeName,t4.goodstypename as goodstypename from pyg_goods T1  ,pyg_brand T2,pyg_goodssize T3, pyg_goodstype T4 where t1.brandId=t2.brandId and    T1.sizeId=t3.goodsSizeId and t1.typeid=t4.goodstypeid";
+
+
+    public void addGoods(Goods goods) {
+
+        SessionFactory.getCurrentSession().saveOrUpdate(goods);
+    }
+
+    public List queryBrand() {
+
+
+        List list= SessionFactory.getCurrentSession().createQuery("FROM Brand").list();
+        return list;
+    }
+
+
+    public Goods hxGoods(String hql) {
+        Goods  goodss = (Goods) SessionFactory.getCurrentSession().createQuery(hql).uniqueResult();
+        return goodss;
+    }
+    public void delGoods(String hql) {
+        SessionFactory.getCurrentSession().createQuery(hql).executeUpdate();
+    }
+
+    public void addBrand(Brand brand) {
+
+        new SimpleDateFormat();
+        SessionFactory.getCurrentSession().saveOrUpdate(brand);
+    }
+
+    public void addSize(GoodsSzie size) {
+        SessionFactory.getCurrentSession().saveOrUpdate(size);
+    }
+
+    public List<BigType> queryBigType() {
+        List list= SessionFactory.getCurrentSession().createQuery("FROM BigType").list();
+        return list;
+    }
+
+    public List<SmallType> querySmallType(Integer bigTypeid) {
+        String hql="SELECT t.smallTypeId as smallTypeId,t.smallTypeName as smallTypeName FROM pyg_smalltype t,pyg_bigtype d WHERE t.bigTypeId =d.bigTypeId AND t.bigTypeId="+bigTypeid;
+        List list= SessionFactory.openSession().createSQLQuery(hql).setResultTransformer(Transformers.ALIAS_TO_ENTITY_MAP).list();
+
+        return list;
+    }
+
+
+
+    public List<GoodsSzie> querySize() {
+        List list= SessionFactory.getCurrentSession().createQuery("FROM GoodsSzie").list();
+        return list;
+    }
+
+
+    public List queryGoods(Integer merchantId) {
+        String hql="SELECT T1.goodsid AS goodsid,T2.smallTypeName as smallTypeName,T3.bigTypeName as bigTypeName,T3.bigTypeId as bigTypeId,T1.goodsAuditState AS goodsAuditState,T1.goodsPrice AS goodsPrice,T1.goodsimg AS goodsimg,T1.goodsname AS goodsname,T1.sizeId AS sizeId,T1.goodsScript AS goodsScript,T4.brandName AS brandName ,T5.goodsSizeName AS goodsSizeName FROM pyg_goods T1,pyg_smalltype T2,pyg_bigtype T3,pyg_brand T4,pyg_goodssize T5 WHERE  T1.smallTypeId=T2.smallTypeId AND T2.bigTypeId=T3.bigTypeId AND T1.brandId=T4.brandId AND T1.sizeId=T5.goodsSizeId AND T1.merchantId="+merchantId;
 
         List list= SessionFactory.openSession().createSQLQuery(hql).setResultTransformer(Transformers.ALIAS_TO_ENTITY_MAP).list();
         return list;
     }
+
 }
-
-
-
-
-
-
-
 
--- webapp/src/main/java/com/jk/action/SyzAction.java
+++ webapp/src/main/java/com/jk/action/SyzAction.java
@@ -1,12 +1,19 @@
 package com.jk.action;
 
-import com.jk.pojo.Goods;
+
+import com.jk.pojo.*;
 import com.jk.service.SyzService;
+import com.jk.util.FileUtil;
+import org.apache.struts2.ServletActionContext;
 import org.apache.struts2.convention.annotation.Action;
 import org.apache.struts2.convention.annotation.Namespace;
 import org.apache.struts2.convention.annotation.ParentPackage;
 import org.springframework.beans.factory.annotation.Autowired;
 
+
+import java.io.File;
+import java.io.IOException;
+import java.io.PrintWriter;
 import java.util.List;
 
 @Namespace(value="/syz")
@@ -15,22 +22,230 @@ public class SyzAction extends BaseAction{
 
     @Autowired
     private SyzService syzService;
-
-    private Goods goods=new Goods();
     private Integer goodsid;
+
+    private Integer bigTypeid;
+    private Integer merchantId;
+
+    private Integer smallTypeid;
+
+
     private List list;
+    private Goods goods = new  Goods();
+    private Brand brand =new Brand();
+    private  Merchant Merchant =new Merchant();
+    private GoodsSzie size =new GoodsSzie();
+    private Integer brandId;
+    private String uploadFileFileName;
+    private File uploadFile;
+    private BigType bigtype = new  BigType();
+    private SmallType smallType = new  SmallType();
 
     @Action(value="/queryGoods")
     public void queryGoods(){
-
-        list = syzService.queryGoods();
-
+        Merchant= (Merchant) ServletActionContext.getRequest().getSession().getAttribute("merchant");
+        list = syzService.queryGoods(Merchant.getMerchantId());
         super.writeJson(list);
+
+    }
+
+
+    @Action(value="/queryBrand")
+    public void queryBrand(){
+       List<Brand> list = syzService.queryBrand();
+        super.writeJson(list);
+
+    }
+    @Action(value="/querySize")
+    public void querySize(){
+        List<GoodsSzie> list = syzService.querySize();
+        super.writeJson(list);
+
+    }
+    @Action(value="/queryBigType")
+    public void queryBigType(){
+       List<BigType> list = syzService.queryBigType();
+        super.writeJson(list);
+
+    }
+    @Action(value="/querySmallType")
+    public void querySmallType(){
+
+       List<SmallType> list = syzService.querySmallType(bigTypeid);
+        super.writeJson(list);
+
+    }
+
+    @Action(value="/addGoods")
+    public void addGoods(){
+        Merchant= (Merchant) ServletActionContext.getRequest().getSession().getAttribute("merchant");
+        goods.setMerchantId(Merchant.getMerchantId());
+        syzService.addGoods(goods);
+        super.writeJson("Success");
+     }
+    @Action(value="/addBrand")
+    public void adBrand(){
+
+        syzService.addBrand(brand);
+        super.writeJson("Success");
+     }
+    @Action(value="/addSize")
+    public void addSize(){
+
+        syzService.addSize(size);
+        super.writeJson("Success");
+     }
+
+
+    @Action(value="/hxGoods")
+    public void  hxGoods(){
+        Goods goods = syzService.hxGoods(goodsid);
+        super.writeJson(goods);
+    }
+    @Action(value="/delGoods")
+    public void  delGoods(){
+        syzService.delGoods(goodsid);
+        super.writeJson("delAllSuccess");
+    }
+    @Action(value="/updatedGoods")
+    public void updatedGoods(){
+        Merchant= (Merchant) ServletActionContext.getRequest().getSession().getAttribute("merchant");
+        goods.setMerchantId(Merchant.getMerchantId());
+        syzService.addGoods(goods);
+        super.writeJson("Success");
+
+    }
+    //上传图片
+    @Action("/scwj")
+    public void scwj(){
+        PrintWriter writer = null;
+        try {
+            String upLoadFilePath = FileUtil.upLoad(uploadFile, uploadFileFileName, "uploadimg");
+            ServletActionContext.getResponse().setCharacterEncoding("utf-8");
+            writer = ServletActionContext.getResponse().getWriter();
+            writer.write(upLoadFilePath);
+        } catch (IOException e) {
+            e.printStackTrace();
+        }finally{
+            if(writer != null){
+                writer.close();
+            }
+        }
+    }
+
+    public BigType getBigtype() {
+        return bigtype;
+    }
+
+    public void setBigtype(BigType bigtype) {
+        this.bigtype = bigtype;
+    }
+
+    public SmallType getSmallType() {
+        return smallType;
+    }
+
+    public void setSmallType(SmallType smallType) {
+        this.smallType = smallType;
+    }
+
+    public Integer getBigTypeid() {
+        return bigTypeid;
+    }
+
+    public void setBigTypeid(Integer bigTypeid) {
+        this.bigTypeid = bigTypeid;
+    }
+
+    public Integer getSmallTypeid() {
+        return smallTypeid;
+    }
+
+    public void setSmallTypeid(Integer smallTypeid) {
+        this.smallTypeid = smallTypeid;
+    }
+
+
+
+    public GoodsSzie getSize() {
+        return size;
+    }
+
+    public void setSize(GoodsSzie size) {
+        this.size = size;
+    }
+
+    public Integer getBrandId() {
+        return brandId;
+    }
+
+    public void setBrandId(Integer brandId) {
+        this.brandId = brandId;
+    }
+
+    public Brand getBrand() {
+        return brand;
+    }
+
+    public void setBrand(Brand brand) {
+        this.brand = brand;
+    }
+
+
+
+    public String getUploadFileFileName() {
+        return uploadFileFileName;
+    }
+
+    public void setUploadFileFileName(String uploadFileFileName) {
+        this.uploadFileFileName = uploadFileFileName;
+    }
+
+    public File getUploadFile() {
+        return uploadFile;
+    }
+
+    public void setUploadFile(File uploadFile) {
+        this.uploadFile = uploadFile;
+    }
+
+    public Integer getGoodsid() {
+        return goodsid;
+    }
+
+    public void setGoodsid(Integer goodsid) {
+        this.goodsid = goodsid;
     }
 
     public List getList() {
         return list;
     }
 
+    public void setList(List list) {
+        this.list = list;
+    }
+
+    public Goods getGoods() {
+        return goods;
+    }
+
+    public void setGoods(Goods goods) {
+        this.goods = goods;
+    }
+    public Integer getMerchantId() {
+        return merchantId;
+    }
+
+    public void setMerchantId(Integer merchantId) {
+        this.merchantId = merchantId;
+    }
+
+    public com.jk.pojo.Merchant getMerchant() {
+        return Merchant;
+    }
+
+    public void setMerchant(com.jk.pojo.Merchant merchant) {
+        Merchant = merchant;
+    }
 
 }
