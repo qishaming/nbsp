e26b41c608f22c56c3bec8e642ced0decb7fa1cc
asus <624689958@qq.com>
2018-05-17 23:05:19 +0800

    echarts范例

---
+++ webapp/src/main/java/com/jk/action/JumpAction.java
@@ -0,0 +1,9 @@
+package com.jk.action;
+
+import org.apache.struts2.convention.annotation.Namespace;
+import org.apache.struts2.convention.annotation.ParentPackage;
+
+@Namespace(value="/jump")
+@ParentPackage("basePackage")
+public class JumpAction {
+}
